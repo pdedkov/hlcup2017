9428c264da008cdb9dd8a20678d41f55dacb2b2f
Pavel E. Dedkov <pavel.dedkov@gmail.com>
2017-08-30 21:58:01 +0300

    time: 246.275863

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git commit -a -m "next"
-docker build . -t pdedkov/hlcup && docker tag pdedkov/hlcup stor.highloadcup.ru/travels/sloth_climber && docker push stor.highloadcup.ru/travels/sloth_climber
-
